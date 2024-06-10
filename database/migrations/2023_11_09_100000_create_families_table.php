70046a5834d0e392ef98303f210d114926d71fe7
deviengeorge99 <deviengeorge99@gmail.com>
2024-06-10 22:07:38 +0300

    adding

--- database/migrations/2023_11_09_100000_create_families_table.php
+++ database/migrations/2023_11_09_100000_create_families_table.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -27,7 +26,7 @@ return new class extends Migration
 
             // Street Many To One
             // TODO: What is this?
-            $table->foreignId('family_id')->nullable()->constrained()->nullOnDelete();
+            // $table->foreignId('family_id')->nullable()->constrained()->nullOnDelete();
 
             $table->string('building_num')->nullable();
             $table->string('floor_num')->nullable();
--- database/migrations/2023_11_09_113742_create_people_table.php
+++ database/migrations/2023_11_09_113742_create_people_table.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Family;
+use App\Models\SchoolStudentInfo;
+use App\Models\UniversityStudentInfo;
+use App\Models\WorkerInfo;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -30,20 +34,29 @@ return new class extends Migration {
             $table->date('birthday')->nullable();
 
             // Family One To Many
-            $table->foreignId('family_id')->nullable()->constrained()->cascadeOnDelete();
+            // $table->foreignId('family_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Family::class, "family_id")->nullable()->cascadeOnDelete();
             $table->string('family_role')->nullable();
 
             $table->string('title')->nullable();
 
             // worker_info_id
-            $table->foreignId('worker_info_id')->nullable()->constrained(table: "worker_infos")->cascadeOnDelete();
-            $table->unique('worker_info_id');
+            $table->foreignIdFor(WorkerInfo::class, "worker_info_id")->nullable()->constrained()->cascadeOnDelete();
+
+            // $table->foreignId('worker_info_id')->nullable()->constrained(table: "worker_infos")->cascadeOnDelete();
+            // $table->unique('worker_info_id');
+
             // school_student_id
-            $table->foreignId('school_student_info_id')->nullable()->constrained(table: "school_student_infos", column: "id")->cascadeOnDelete();
-            $table->unique('school_student_info_id');
+            $table->foreignIdFor(SchoolStudentInfo::class, "school_student_info_id")->nullable()->constrained()->cascadeOnDelete();
+
+            // $table->foreignId('school_student_info_id')->nullable()->constrained(table: "school_student_infos", column: "id")->cascadeOnDelete();
+            // $table->unique('school_student_info_id');
+
             // university_student_id
-            $table->foreignId('university_student_info_id')->nullable()->constrained(table: "university_student_infos")->cascadeOnDelete();
-            $table->unique('university_student_info_id');
+            $table->foreignIdFor(UniversityStudentInfo::class, "university_student_info_id")->nullable()->constrained()->cascadeOnDelete();
+
+            // $table->foreignId('university_student_info_id')->nullable()->constrained(table: "university_student_infos")->cascadeOnDelete();
+            // $table->unique('university_student_info_id');
 
             $table->timestamps();
         });
